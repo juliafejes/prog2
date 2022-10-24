e93ce12f45df5aa9fc93a47d42249d5b5fe73568
Julia Fejes <jufe9434@enequist.it.uu.se>
2022-10-25 00:49:35 +0200

    kvällsversion

--- person.cpp
+++ person.cpp
@@ -6,8 +6,10 @@ class Person{
 		Person(int);
 		int get();
 		void set(int);
+		long long int fib();
 	private:
 		int age;
+		long long int _fib(int);
 	};
  
 Person::Person(int n){
@@ -22,11 +24,24 @@ void Person::set(int n){
 	age = n;
 	}
 
+long long int Person::person(){
+	return _fib(val);
+	}
+
+long long int Person::_fib(int n){
+	if (n<=1){
+	return n;
+	} else {
+	return (_fib(n-1)+_fib(n-2));
+	}
+	}
+
 
 extern "C"{
 	Person* Person_new(int n) {return new Person(n);}
 	int Person_get(Person* person) {return person->get();}
 	void Person_set(Person* person, int n) {person->set(n);}
+	long long int Person_fib(Person* person) {return person->fib();}
 	void Person_delete(Person* person){
 		if (person){
 			delete person;
